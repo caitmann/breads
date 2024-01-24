a376b2f41ab4d5b00967258143d37b352d1905e0
caitmann <caitlynleemann@gmail.com>
2024-01-23 21:12:31 -0500

    adding index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -15,8 +15,14 @@ breads.get('/new', (req, res) => {
 })
 
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-})
+    if(Bread[req.params.arrayIndex]) {
+        res.render('Show', {
+            bread: Bread[req.params.arrayIndex]
+        })
+        } else {
+            res.send('404')
+        }
+    })
 
 breads.post('/', (req, res) => {
     if(!req.body.image) {
--- server.js
+++ server.js
@@ -8,9 +8,14 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to an awesome app about breads!')
+})
+
+app.get('*', (req, res) => {
+    res.send('404')
 })
 
 app.listen(PORT, () => {

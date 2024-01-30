fc8ea25eaec6a06508b6b33747a6fb5be416b016
caitmann <caitlynleemann@gmail.com>
2024-01-30 17:14:28 -0500

    adding delete form

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -9,6 +10,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to an awesome app about breads!')
@@ -22,5 +24,5 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
 
-const breadsController = require('./controllers/breads_controller')
+const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
